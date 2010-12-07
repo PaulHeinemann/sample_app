c732238207d5d529b33558880f30310e6f1704b1
Paul Heinemann <junkemailaddress3@gmail.com>
2010-12-07 17:49:03 -0500

    added a user factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Paul Heinemann"
+  user.email                  "paul@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
