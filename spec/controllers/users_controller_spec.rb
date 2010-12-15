ec4978b4f40b7c751b529872456b107fac053e6c
Paul Heinemann <junkemailaddress3@gmail.com>
2010-12-15 15:50:12 -0500

    Users index with pagination

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,8 +10,7 @@ describe UsersController do
       it "should deny acccess" do 
         get :index
         response.should redirect_to(signin_path)
-        Factory(:user, :email => "another@example.com")
-        Factory(:user, :email => "another@example.net")
+        
       end
     end
     
@@ -19,6 +18,12 @@ describe UsersController do
       
       before(:each) do
         @user = test_sign_in(Factory(:user))
+        Factory(:user, :email => "another@example.com")
+        Factory(:user, :email => "another@example.net")
+      
+        30.times do
+          Factory(:user, :email => Factory.next(:email))
+        end
       end
       
       it "should be successful" do
@@ -33,9 +38,20 @@ describe UsersController do
       
       it "should have an element for each user" do
         get :index
-        User.all.each do |user|
+        User.paginate(:page => 1).each do |user|
           response.should have_selector('li', :content => user.name)
         end
+      end
+      
+      it "should paginate user" do
+        get :index
+        response.should have_selector("div.pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/users?page=2",
+                                           :content => "2")
+        response.should have_selector("a", :href => "/users?page=2",
+                                                          :content => "Next")
+      
       end
     end
   end
