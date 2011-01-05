2552062b103b38cbece7cfdb3be872ae424262e3
Paul Heinemann <junkemailaddress3@gmail.com>
2011-01-05 15:39:45 -0500

    micropost validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -5,10 +5,10 @@ describe Micropost do
   
   before(:each) do
     @user = Factory(:user)
-    @att = { :content => "lorum ipsum" }
+    @attr = { :content => "lorum ipsum" }
   end
   
-  it "should create and new instance with valid attribute" do
+  it "should create a new instance given valid attributes" do
     @user.microposts.create!(@attr)
   end
   
@@ -25,6 +25,22 @@ describe Micropost do
     it "should have the right associated user" do
       @micropost.user_id.should == @user.id
       @micropost.user.should == @user
+    end
+  end
+  
+  describe "validations" do
+    
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    
+    it "should require non-blank content" do
+      @user.microposts.build(:content => "    ").should_not be_valid
+      
+    end
+    
+    it "should reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid
     end
   end
 end
